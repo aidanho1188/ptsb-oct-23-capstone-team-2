20cd9412e84c84834b4176c9d77dce3247623b1f
aidanho1188 <aaron23657@gmail.com>
2024-03-05 18:01:37 -0500

    cadd frontend route to homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,23 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import * as React from 'react'
+import {QueryClient, QueryClientProvider} from 'react-query'
+import Homepage from './pages/Homepage.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './App.css'
+
+const queryClient = new QueryClient()
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Homepage />,
+  },
+])
 
 function App() {
   return (
-    <div className='bg-blue-500 text-white p-4'>
-      <h1 className='text-3xl'>Welcome to My React App!</h1>
-      <p className='text-lg'>This is a paragraph with Tailwind CSS styles.</p>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   )
 }
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
 
 export default App
---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,5 @@
+function Homepage() {
+  return <div>This is the Homepage</div>
+}
+
+export default Homepage
