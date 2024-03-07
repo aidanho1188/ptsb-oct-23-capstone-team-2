e21f2ab29f4e514f78673bcdcb3b05d7a04ea2a7
Daniela Watanabe <danielarosewatanabe@gmail.com>
2024-03-07 17:14:10 -0600

    added Update Status jsx and css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,17 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 import Homepage from './pages/Homepage.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import UpdateStatus from './pages/UpdateStatus.jsx'
 
 const queryClient = new QueryClient()
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Homepage />,
+  },
+  {
+    path: '/UpdateStatus',
+    element: <UpdateStatus />,
   },
 ])
 
