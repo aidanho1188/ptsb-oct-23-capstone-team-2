22fb8acf35dc6a6aa8573a524451813d7d193f08
aidanho1188 <aaron23657@gmail.com>
2024-03-07 18:51:21 -0500

    refactored code

--- client/src/components/Summary/Summary.jsx
+++ client/src/components/Summary/Summary.jsx
@@ -1,12 +1,4 @@
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '../../../components/ui/table'
+import {Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow} from '../../../components/ui/table'
 import './summary.css'
 
 function Summary() {
