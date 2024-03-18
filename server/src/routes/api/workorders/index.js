678e1b6295988fb5966b9bf9ab9bfc24ed88d90a
aidanho1188 <aaron23657@gmail.com>
2024-03-18 01:53:35 -0400

    Add Awaiting Quote Table component to Dashboard

--- server/src/routes/api/workorders/index.js
+++ server/src/routes/api/workorders/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const workordersOpen = require('./open')
 const onSite = require('./onSite')
+const awaitingQuote = require('./awaitingQuote')
 
 router.use('/open', workordersOpen)
 router.use('/onSite', onSite)
+router.use('/awaitingQuote', awaitingQuote)
 
 module.exports = router
