ce2c6284f82d98c0c6ab0f4e46ae5fd9dd53cb6b
aidanho1188 <aaron23657@gmail.com>
2024-03-21 19:15:32 -0400

    Update table height and remove unused components in Dashboard.jsx

--- client/src/layouts/Dashboard/Dashboard.jsx
+++ client/src/layouts/Dashboard/Dashboard.jsx
@@ -2,12 +2,7 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import TablePlaceholder from '../../components/DashboardPlaceholder/TablePlaceholder'
 import DashboardChart from '../../components/DashboardChart/DashboardChart'
-import OpenWorkorders from '../../components/OpenWorkorders/OpenWorkorders'
-import OnSiteStatus from '../../components/OnSiteStatus/OnSiteStatus'
-import CompletednWO from '../../components/CompletedWO/CompletedWO'
 import './dashboard.css'
-import IncompleteStatus from '../../components/IncompleteStatus/IncompleteStatus'
-import AwaitingQuoteTable from '../../components/AwaitingQuoteTable/AwaitingQuoteTable'
 
 function Dashboard() {
   const [openWorkOrdersData, setOpenWorkOrdersData] = useState()
