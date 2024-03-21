6e6a991cf24186373817efed10648941fcafc70e
aidanho1188 <aaron23657@gmail.com>
2024-03-21 19:07:06 -0400

    Add onClick event to Logo component

--- server/src/services/tokenService.js
+++ server/src/services/tokenService.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const axios = require('axios')
 const getRefreshToken = require('../utils/getRefetchToken.js')
 const saveToken = require('../utils/saveToken.js')
@@ -5,19 +6,14 @@ const sendErrorResponse = require('../utils/errorHandler.js')
 const clientId = process.env.SB_CLIENT_ID
 const clientSecret = process.env.SB_CLIENT_SECRET
 const tokenType = process.env.SB_TOKEN_TYPE
+const grantType = process.env.GRANT_TYPE
+const redirect_uri = process.env.REDIRECT_URI
+const authCode = decodeURIComponent(process.env.SB_AUTH_CODE)
 const currentTime = new Date().getTime()
 const cooldownPeriod = 9 * 60 * 1000
 let lastFetchTime = 0
 
 async function refetchAccessToken() {
-  // if (isOnCooldown()) {
-  //   console.log('Token is still valid')
-  //   return
-  // } else {
-  //   lastFetchTime = currentTime
-  //   console.log(`lastFetchTime: ${lastFetchTime} currentTime: ${currentTime} cooldownPeriod: ${cooldownPeriod}`)
-  // }
-
   try {
     const body = `grant_type=refresh_token&refresh_token=${await getRefreshToken('sandbox')}`
 
@@ -39,8 +35,30 @@ async function refetchAccessToken() {
   }
 }
 
+async function fetchToken() {
+  try {
+    const body = `grant_type=${grantType}&code=${authCode}&redirect_uri=${redirect_uri}`
+
+    const response = await axios.post('https://sb2login.servicechannel.com/oauth/token', body, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+      },
+    })
+
+    const data = response.data
+    const accessToken = data.access_token
+    const refreshToken = data.refresh_token
+    console.log(data)
+    await saveToken(tokenType, accessToken, refreshToken)
+  } catch (error) {
+    const errorResponse = sendErrorResponse(error)
+    console.error('Error:', errorResponse)
+  }
+}
+
 function isOnCooldown() {
   return currentTime - lastFetchTime < cooldownPeriod
 }
 
-module.exports = {refetchAccessToken}
+module.exports = {refetchAccessToken, fetchToken}
