b1ffa619a12d8005339bfffcbf1f6c1f270d6841
DavidBlumenshine <119887262+DavidBlumenshine@users.noreply.github.com>
2024-04-04 19:20:18 -0500

    ADD 66 recolored dashboard chart, add y-axis key

--- client/src/components/DashboardChart/DashboardChart.jsx
+++ client/src/components/DashboardChart/DashboardChart.jsx
@@ -1,47 +1,70 @@
-import {BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar} from 'recharts'
-import './dashboardChart.css'
-import {useEffect} from 'react'
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+} from "recharts";
+import "./dashboardChart.css";
+import { useEffect } from "react";
 
 function DashboardChart() {
   let data = [
     {
-      name: 'Open',
-      'Work Orders': 15,
+      name: "Open",
+      "Work Order Status": 15,
+      fill: "#5FC6FF",
     },
     {
-      name: 'On Site',
-      'Work Orders': 5,
+      name: "Awaiting Quote",
+      "Work Order Status": 2,
+      fill: "#FFC154",
     },
     {
-      name: 'In Progress',
-      'Work Orders': 8,
+      name: "On Site",
+      "Work Order Status": 5,
+      fill: "#FFAD54",
     },
     {
-      name: 'Incomplete',
-      'Work Orders': 3,
+      name: "In Progress",
+      "Work Order Status": 8,
+      fill: "#FF5F5F",
     },
     {
-      name: 'Completed',
-      'Work Orders': 12,
+      name: "Incomplete",
+      "Work Order Status": 3,
+      fill: "#B55FFF",
     },
     {
-      name: 'Awaiting Quote',
-      'Work Orders': 2,
+      name: "Completed",
+      "Work Order Status": 12,
+      fill: "#61CE99",
     },
-  ]
+  ];
 
   return (
-    <div className='dashboard-chart'>
+    <div className="dashboard-chart">
       <BarChart width={900} height={250} data={data}>
-        <CartesianGrid strokeDasharray='3 3' fill='#00541A' />
-        <XAxis dataKey='name' stroke='white' />
-        <YAxis stroke='white' />
+        <CartesianGrid strokeDasharray="3 3" fill="#4f4f4f" />
+        <XAxis dataKey="name" stroke="white" />
+        <YAxis
+          stroke="white"
+          label={{
+            value: "No. Work Orders",
+            angle: -90,
+            position: "insideBottomLeft",
+            fill: "white",
+            offset: 15,
+          }}
+        />{" "}
         <Tooltip />
         <Legend />
-        <Bar dataKey='Work Orders' fill='#61CE70' />
+        <Bar dataKey="Work Order Status" fill="#ffff" />
       </BarChart>
     </div>
-  )
+  );
 }
 
-export default DashboardChart
+export default DashboardChart;
