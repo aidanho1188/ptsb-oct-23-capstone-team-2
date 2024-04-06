a88ee5e549d864d722de565f402eec4475b216b9
aidanho1188 <aaron23657@gmail.com>
2024-04-06 03:48:41 -0400

    Refactor file paths and import statements in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,59 +1,58 @@
-import * as React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
-import Homepage from "./pages/Homepage/Homepage.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
-import UpdateStatusPage from "./pages/UpdateStatusPage/UpdateStatusPage.jsx";
-import WorkorderCheckinPage from "./pages/WorkorderCheckinPage/WorkorderCheckinPage.jsx";
-import WorkorderCheckoutPage from "./pages/WorkorderCheckoutPage/WorkorderCheckoutPage.jsx";
-import LoginPage from "./pages/LoginPage/LoginPage.jsx";
-import CreateWorkorderPage from "./pages/CreateWorkorderPage/CreateWorkorderPage.jsx";
-import WorkActivityPage from "./pages/WorkActivityPage/WorkActivity.jsx";
+import * as React from 'react'
+import {QueryClient, QueryClientProvider} from 'react-query'
+import Homepage from './pages/Homepage/Homepage.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import './App.css'
+import UpdateStatusPage from './pages/UpdateStatusPage/UpdateStatusPage.jsx'
+import WorkorderCheckinPage from './pages/WorkorderCheckinPage/WorkorderCheckinPage.jsx'
+import WorkorderCheckoutPage from './pages/WorkorderCheckoutPage/WorkorderCheckoutPage.jsx'
+import LoginPage from './pages/LoginPage/LoginPage.jsx'
+import CreateWorkorderPage from './pages/CreateWorkorderPage/CreateWorkorderPage.jsx'
+import WorkActivityPage from './pages/WorkActivityPage/WorkActivity.jsx'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Homepage />,
   },
   {
-    path: "/update-status",
+    path: '/update-status',
     element: <UpdateStatusPage />,
   },
   {
-    path: "/workorder-checkin",
+    path: '/workorder-checkin',
     element: <WorkorderCheckinPage />,
   },
   {
-    path: "/workorder-checkout",
+    path: '/workorder-checkout',
     element: <WorkorderCheckoutPage />,
   },
   {
-    path: "/login",
+    path: '/login',
     element: <LoginPage />,
   },
   {
-    path: "/create-workorder",
+    path: '/create-workorder',
 
     element: <CreateWorkorderPage />,
   },
   {
-    path: "/work-activity",
+    path: '/work-activity',
     element: <WorkActivityPage />,
   },
   {
-    path: "/workorder-create",
+    path: '/workorder-create',
     element: <CreateWorkorderPage />,
   },
-]);
-
+])
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
     </QueryClientProvider>
-  );
+  )
 }
 
-export default App;
+export default App
