3395bfd7213a5e43d4a1835327a9ca54db604583
DavidBlumenshine <119887262+DavidBlumenshine@users.noreply.github.com>
2024-04-10 21:45:34 -0500

    add bar chart radius styling to round edges

--- client/src/components/DashboardChart/DashboardChart.jsx
+++ client/src/components/DashboardChart/DashboardChart.jsx
@@ -1,66 +1,107 @@
-import {BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar} from 'recharts'
-import './dashboardChart.css'
-import {useEffect} from 'react'
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+} from "recharts";
+import "./dashboardChart.css";
+import { useEffect } from "react";
 
-function DashboardChart({isLoading, open, onSite, incomplete, missedCheckIn, awaitingQuote, completed}) {
+function DashboardChart({
+  isLoading,
+  open,
+  onSite,
+  incomplete,
+  missedCheckIn,
+  awaitingQuote,
+  completed,
+}) {
   let data = [
     {
-      name: 'Open',
+      name: "Open",
       Open: open ? open.length : 0,
-      fill: '#5FC6FF',
+      fill: "#5FC6FF",
     },
     {
-      name: 'Awaiting Quote',
-      'Awaiting Quote': awaitingQuote ? awaitingQuote.length : 0,
-      fill: '#000000',
+      name: "Awaiting Quote",
+      "Awaiting Quote": awaitingQuote ? awaitingQuote.length : 0,
+      fill: "#F48FB1",
     },
     {
-      name: 'On Site',
-      'On Site': onSite ? onSite.length : 0,
-      fill: '#000080',
+      name: "On Site",
+      "On Site": onSite ? onSite.length : 0,
+      fill: "#000080",
     },
     {
-      name: 'In Progress',
-      'In Progress': onSite ? onSite.length : 0,
-      fill: '#8B0000',
+      name: "In Progress",
+      "In Progress": onSite ? onSite.length : 0,
+      fill: "#8B0000",
     },
     {
-      name: 'Incomplete',
+      name: "Incomplete",
       Incomplete: incomplete ? incomplete.length : 0,
-      fill: '#FFD700',
+      fill: "#FFD700",
     },
     {
-      name: 'Completed',
+      name: "Completed",
       Completed: completed ? completed.length : 0,
-      fill: '#800080',
+      fill: "#800080",
     },
-  ]
+  ];
 
   return (
     <BarChart width={1500} height={450} data={data}>
-      <CartesianGrid strokeDasharray='1 1' fill='#4f4f4f' />
-      <XAxis dataKey='name' stroke='white' />
+      <CartesianGrid strokeDasharray="1 2" fill="#4f4f4f" />
+      <XAxis dataKey="name" stroke="white" />
       <YAxis
-        stroke='white'
+        stroke="white"
         label={{
-          value: 'No. Work Orders',
+          value: "No. Work Orders",
           angle: -90,
-          position: 'insideBottomLeft',
-          fill: 'black',
+          position: "insideBottomLeft",
+          fill: "black",
           offset: 15,
           fontSize: 18,
         }}
       />
       <Tooltip />
       <Legend />
-      <Bar dataKey='Open' stackId='a' fill='#5FC6FF' />
-      <Bar dataKey='Awaiting Quote' stackId='a' fill='#000000' />
-      <Bar dataKey='On Site' stackId='a' fill='#000080' />
-      <Bar dataKey='In Progress' stackId='a' fill='#8B0000' />
-      <Bar dataKey='Incomplete' stackId='a' fill='#FFD700' />
-      <Bar dataKey='Completed' stackId='a' fill='#800080' />
+      <Bar dataKey="Open" stackId="a" fill="#5FC6FF" radius={[10, 10, 0, 0]} />
+      <Bar
+        dataKey="Awaiting Quote"
+        stackId="a"
+        fill="#F48FB1"
+        radius={[10, 10, 0, 0]}
+      />
+      <Bar
+        dataKey="On Site"
+        stackId="a"
+        fill="#000080"
+        radius={[10, 10, 0, 0]}
+      />
+      <Bar
+        dataKey="In Progress"
+        stackId="a"
+        fill="#8B0000"
+        radius={[10, 10, 0, 0]}
+      />
+      <Bar
+        dataKey="Incomplete"
+        stackId="a"
+        fill="#FFD700"
+        radius={[10, 10, 0, 0]}
+      />
+      <Bar
+        dataKey="Completed"
+        stackId="a"
+        fill="#800080"
+        radius={[10, 10, 0, 0]}
+      />
     </BarChart>
-  )
+  );
 }
 
-export default DashboardChart
+export default DashboardChart;
