8d090644a6f86be820876989c2e00855b0d3e3fa
aidanho1188 <aaron23657@gmail.com>
2024-08-31 00:57:50 -0400

    Add carousel component and also more content to about page

---
+++ client/src/components/TeamCarousel/TeamCarousel.jsx
@@ -0,0 +1,85 @@
+import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from '@/components/ui/carousel'
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from '@/components/ui/card'
+import {Separator} from '@/components/ui/separator'
+
+function TeamCarousel() {
+  return (
+    <Carousel
+      opts={{
+        dragFree: true,
+        loop: true,
+        align: 'center',
+        slidesToScroll: 'auto',
+      }}
+      className='w-full'
+    >
+      <CarouselContent className='-ml-2 md:-ml-4'>
+        <CarouselItem className='basis-1/4 pl-2 md:pl-4'>
+          <Card>
+            <CardHeader>
+              <img width={280} height={300} className='carousel-image' alt='Matt Picture Here' src='../src/assets/matt.webp' />
+              <CardTitle>Matt Nolan</CardTitle>
+              <CardDescription>Product Owner</CardDescription>
+            </CardHeader>
+            <Separator className='separator' />
+            <CardContent>
+              <p>
+                Outside Unlimited <br />
+                Administrative Account Manager
+              </p>
+            </CardContent>
+          </Card>
+        </CarouselItem>
+
+        <CarouselItem className='basis-1/4 pl-2 md:pl-4'>
+          <Card>
+            <CardHeader>
+              <img width={280} height={300} className='carousel-image' alt='Aidan Picture Here' src='../src/assets/aidan.webp' />
+              <CardTitle>Aidan Ho</CardTitle>
+              <CardDescription>Project Leader</CardDescription>
+            </CardHeader>
+            <Separator className='separator' />
+            <CardContent>
+              <p>Location: Biddeford, Maine</p>
+              <p>Hobbies: Martial Arts, Guitar, Digital Arts</p>
+            </CardContent>
+          </Card>
+        </CarouselItem>
+
+        <CarouselItem className='basis-1/4 pl-2 md:pl-4'>
+          <Card>
+            <CardHeader>
+              <img width={280} height={300} className='carousel-image' alt='Daniela Picture Here' src='../src/assets/daniela.webp' />
+              <CardTitle>Daniela Watanabe</CardTitle>
+              <CardDescription>Fullstack Developer</CardDescription>
+            </CardHeader>
+            <Separator className='separator' />
+            <CardContent>
+              <p>Location: Wichita, Kansas</p>
+              <p>Hobbies: Gaming, Cooking, Crochet</p>
+            </CardContent>
+          </Card>
+        </CarouselItem>
+
+        <CarouselItem className='basis-1/4 pl-2 md:pl-4'>
+          <Card>
+            <CardHeader>
+              <img width={280} height={300} className='carousel-image' alt='David Picture Here' src='../src/assets/david.webp' />
+              <CardTitle>David Blumenshine</CardTitle>
+              <CardDescription>Fullstack Developer</CardDescription>
+            </CardHeader>
+            <Separator className='separator' />
+            <CardContent>
+              <p>Location: Wichita, Kansas</p>
+              <p>Hobbies: Cycling, Painting, Coding</p>
+            </CardContent>
+          </Card>
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
+
+export default TeamCarousel
--- client/src/layouts/AboutPageLayout/AboutPageLayout.jsx
+++ client/src/layouts/AboutPageLayout/AboutPageLayout.jsx
@@ -1,29 +1,22 @@
 import './aboutPageLayout.css'
 import FAQ from '../../components/FAQ/FAQ'
 import {Separator} from '@/components/ui/separator'
+import TeamCarousel from '../../components/TeamCarousel/TeamCarousel'
 
 function AboutPageLayout() {
   return (
     <div className='about-page-content'>
       <h2>About Us</h2>
       <div className='about-page-content-block'>
-        <img className='about-page-content-block-image' alt='Picture Here' />
+        <img className='about-page-content-block-image' alt='Picture Here' src='../src/assets/aidaviela.webp' />
         <p>Our mission is to provide a user-friendly and efficient workorder management solution that helps businesses streamline their operations and improve productivity. </p>
-      </div>
-
-      <Separator />
-      <h2>Our Team</h2>
-      <div className='about-page-content-block'>
-        <img className='about-page-content-block-image' alt='Picture Here' />
-        <img className='about-page-content-block-image' alt='Picture Here' />
-        <img className='about-page-content-block-image' alt='Picture Here' />
       </div>
 
       <Separator />
       <h2>What is the purpose?</h2>
       <div className='about-page-content-block'>
         <p>This app is built for Outside Unlimited to help manage work orders without the need for using the &quot;Service Channel&quot; UI.</p>
-        <img className='about-page-content-block-image' alt='Picture Here' />
+        <img className='about-page-content-block-image' alt='Picture Here' src='../src/assets/outsideunlimited_logo.jpeg' />
       </div>
 
       <Separator />
@@ -35,6 +28,12 @@ function AboutPageLayout() {
         <p>More texts</p>
         <img className='about-page-content-block-image' alt='Picture Here' />
         <p>and more texts</p>
+      </div>
+
+      <Separator />
+      <h2>Our Team</h2>
+      <div className='about-page-content-team-block'>
+        <TeamCarousel />
       </div>
 
       <Separator />
