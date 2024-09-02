f105cc1e0f3fd13a26e03d1bc5627ec9807972e0
aidanho1188 <aaron23657@gmail.com>
2024-09-02 18:46:03 -0400

    add gifs for how it work

--- client/src/layouts/AboutPageLayout/AboutPageLayout.jsx
+++ client/src/layouts/AboutPageLayout/AboutPageLayout.jsx
@@ -1,10 +1,15 @@
 import './aboutPageLayout.css'
-import React, {useState} from 'react'
+import {useState} from 'react'
 import FAQ from '../../components/FAQ/FAQ'
 import {Separator} from '@/components/ui/separator'
 import TeamCarousel from '../../components/TeamCarousel/TeamCarousel'
 import aidavielaImage from '../../assets/aidaviela.jpeg'
 import outsideunlimitedImage from '../../assets/outsideunlimited.jpg'
+import checkin from '../../assets/checkin.gif'
+import checkout from '../../assets/checkout.gif'
+import createWorkOrder from '../../assets/createWorkOrder.gif'
+import updateStatus from '../../assets/updateStatus.gif'
+import workOrderActivity from '../../assets/workOrderActivity.gif'
 
 function AboutPageLayout() {
   const [activePage, setActivePage] = useState('dashboard')
@@ -53,13 +58,33 @@ function AboutPageLayout() {
         <div className='how-it-works-content'>
           {activePage === 'dashboard' && <p>The Dashboard gives you an overview of the app's key features and current status. It's the central hub for accessing other pages and monitoring general activity. It displays information about work orders, including their status such as open, on site, incomplete, approved proposals, awaiting for quote, and completed. In addition, it has a graph plotted on this data for an overview.</p>}
 
-          {activePage === 'check-in-out' && <p>Use the Check-In and Check-Out pages to manage specific work orders. Check in or check out a work order to update its status accordingly.</p>}
+          {activePage === 'check-in-out' && (
+            <p>
+              Use the Check-In and Check-Out pages to manage specific work orders. Check in or check out a work order to update its status accordingly.
+              <img src={checkin} alt='Check-In and Check-Out GIF' />
+            </p>
+          )}
 
-          {activePage === 'create-work-order' && <p>The Create Work Order page allows you to generate new work orders. Fill in the necessary details, and the system will create a work order and assign a unique ID.</p>}
+          {activePage === 'create-work-order' && (
+            <p>
+              The Create Work Order page allows you to generate new work orders. Fill in the necessary details, and the system will create a work order and assign a unique ID.
+              <img src={createWorkOrder} alt='Create Work Order GIF' />
+            </p>
+          )}
 
-          {activePage === 'update-status' && <p>On the Update Status page, you can change the status of any work order. This page also records the status update in our database for future reference.</p>}
+          {activePage === 'update-status' && (
+            <p>
+              On the Update Status page, you can change the status of any work order. This page also records the status update in our database for future reference.
+              <img src={updateStatus} alt='Update Status GIF' />
+            </p>
+          )}
 
-          {activePage === 'work-order-activity' && <p>Visit the Work Order Activity page to view all activities related to a specific work order. This includes a log of changes and actions taken on that work order.</p>}
+          {activePage === 'work-order-activity' && (
+            <p>
+              Visit the Work Order Activity page to view all activities related to a specific work order. This includes a log of changes and actions taken on that work order.
+              <img src={workOrderActivity} alt='Work Order Activity GIF' />
+            </p>
+          )}
         </div>
       </div>
 
