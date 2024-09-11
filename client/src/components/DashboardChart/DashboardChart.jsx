decf8734ecde9ff8328ab5b4e764c49406cf6469
aidanho1188 <aaron23657@gmail.com>
2024-09-11 14:26:48 -0400

    update statistic and chart name

--- client/src/components/DashboardChart/DashboardChart.jsx
+++ client/src/components/DashboardChart/DashboardChart.jsx
@@ -17,6 +17,7 @@ const chartConfig = {
 const getCurrentWeekDates = () => {
   const currentDate = new Date()
   const dayOfWeek = currentDate.getDay()
+  currentDate.setDate(currentDate.getDate() - 28)
   // current date - day of week + (if sunday then -6 else +1) = monday
   const diffToMonday = currentDate.getDate() - dayOfWeek + (dayOfWeek === 0 ? -6 : 1)
 
@@ -38,21 +39,28 @@ const getCurrentWeekDates = () => {
 }
 
 export function DashboardChart({isLoading, open, onSite, incomplete, missedCheckIn, awaitingQuote, completed}) {
+  const openSize = open ? open.length : 0
+  const onSiteSize = onSite ? onSite.length : 0
+  const incompleteSize = incomplete ? incomplete.length : 0
+  const missedCheckInSize = missedCheckIn ? missedCheckIn.length : 0
+  const awaitingQuoteSize = awaitingQuote ? awaitingQuote.length : 0
+  const completedSize = completed ? completed.length : 0
+
   const chartData = [
-    {status: 'Open', total: open ? open.length : 0},
-    {status: 'On Site', total: onSite ? onSite.length : 0},
-    {status: 'Incomplete', total: incomplete ? incomplete.length : 0},
-    {status: 'Approved Proposals', total: missedCheckIn ? missedCheckIn.length : 0},
-    {status: 'Waiting For Quote', total: awaitingQuote ? awaitingQuote.length : 0},
-    {status: 'Completed', total: completed ? completed.length : 0},
+    {status: 'Open', total: openSize},
+    {status: 'On Site', total: onSiteSize},
+    {status: 'Incomplete', total: incompleteSize},
+    {status: 'Approved Proposals', total: missedCheckInSize},
+    {status: 'Waiting For Quote', total: awaitingQuoteSize},
+    {status: 'Completed', total: completedSize},
   ]
 
   const {monday, sunday} = getCurrentWeekDates()
-
+  const totalWorkOrders = openSize + onSiteSize + incompleteSize + missedCheckInSize + awaitingQuoteSize + completedSize
   return (
     <Card className='dashboard-chart-container'>
       <CardHeader>
-        <CardTitle>Bar Chart - Label</CardTitle>
+        <CardTitle>Work Orders Chart</CardTitle>
         <CardDescription>{`${monday} - ${sunday}`}</CardDescription>
       </CardHeader>
       <CardContent className='dashboard-chart'>
@@ -75,9 +83,9 @@ export function DashboardChart({isLoading, open, onSite, incomplete, missedCheck
       </CardContent>
       <CardFooter className='flex-col items-start gap-2 text-sm'>
         <div className='flex gap-2 font-medium leading-none'>
-          Trending up by 5.2% this status <TrendingUp className='h-4 w-4' />
+          Total work orders this week: {totalWorkOrders} <TrendingUp className='h-4 w-4' />
         </div>
-        <div className='leading-none text-muted-foreground'>Showing total visitors for the last 6 statuss</div>
+        <div className='leading-none text-muted-foreground'>Showing the total number of work orders for 6 statuses from 4 weeks ago</div>
       </CardFooter>
     </Card>
   )
